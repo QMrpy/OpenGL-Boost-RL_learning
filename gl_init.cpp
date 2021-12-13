a75f7b823c0546451fd1deb8a79096e4a7b28f6a
QMrpy <rupayan178@gmail.com>
2021-12-13 23:37:19 +0530

    Build, but not running on WSL2 due to X11 problems

---
+++ gl_init.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+
+void error_callback(int error, const char* description) {
+    std::cout << "Error Details:" << std::endl;
+    std::cout << description << std::endl;
+}
+
+int main() {
+    glfwSetErrorCallback(error_callback);
+    
+    if (!glfwInit()) {
+        std::cout << "Failed to initialize GLFW." << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4); 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
+
+    GLFWwindow* window; 
+    window = glfwCreateWindow(1024, 768, "Hello World!", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to open GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window); 
+    glewExperimental = true; 
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Failed to initialize GLEW." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+    do {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+
+    glfwTerminate();
+    return 0;
+}
